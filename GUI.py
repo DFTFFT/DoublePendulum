9bdb8ac3356c540f685259c60916f5a6877542fb
hz <DFTFFT@163.com>
2016-02-20 02:01:13 -0800

    add function to get input data from GUI

--- GUI.py
+++ GUI.py
@@ -6,8 +6,10 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
 import sys
+from PyQt4 import QtCore, QtGui
+from double_pendulum import DoublePendulum
+
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -24,24 +26,55 @@ except AttributeError:
         return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_MainWindow(QtGui.QMainWindow):
+    """ The main window class"""
+
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
+        
+        # member variables definition
+        self.M1 = 0.0
+        self.M2 = 0.0
+        self.L1 = 0.0
+        self.L2 = 0.0
+
+        self.th1 = 0.0
+        self.th2 = 0.0
+        self.w1 = 0.0
+        self.w2 = 0.0
+
+        self.ts = 0.0
+        self.te = 0.0
+        self.dt = 0.0
+
+        # setup timer
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.timerCallback)
+        self.timer_count = 0
+        self.current_time = 0.0
+
         # Setup UI widgets
         self.setupUi(self)
-        
+
     def setupUi(self, MainWindow):
+        # mainwindow
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(1203, 694)
+
+        # centralWidget
         self.centralWidget = QtGui.QWidget(MainWindow)
         self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
-        self.vtk_widget = QtGui.QWidget(self.centralWidget)
-        self.vtk_widget.setGeometry(QtCore.QRect(320, 10, 431, 581))
-        self.vtk_widget.setObjectName(_fromUtf8("vtk_widget"))
+        MainWindow.setCentralWidget(self.centralWidget)
+
+        # left setting panel
+
+        # frame
         self.settingFrame = QtGui.QFrame(self.centralWidget)
         self.settingFrame.setGeometry(QtCore.QRect(10, 10, 301, 581))
         self.settingFrame.setFrameShape(QtGui.QFrame.StyledPanel)
         self.settingFrame.setFrameShadow(QtGui.QFrame.Raised)
         self.settingFrame.setObjectName(_fromUtf8("settingFrame"))
+
+        # system parameter group
         self.groupBox_system = QtGui.QGroupBox(self.settingFrame)
         self.groupBox_system.setGeometry(QtCore.QRect(20, 20, 261, 121))
         self.groupBox_system.setStyleSheet(_fromUtf8("QGroupBox{border: 1px solid rgb(192, 192, 192); border-radius:9px; font: bold}+ QGroupBox::title{subcontrol-origin: margin; left: 10px; padding: 0px 3px 0 3px}"))
@@ -70,6 +103,8 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.lineEdit_L2 = QtGui.QLineEdit(self.groupBox_system)
         self.lineEdit_L2.setGeometry(QtCore.QRect(170, 80, 71, 27))
         self.lineEdit_L2.setObjectName(_fromUtf8("lineEdit_L2"))
+
+        # initial condition group
         self.groupBox_init = QtGui.QGroupBox(self.settingFrame)
         self.groupBox_init.setGeometry(QtCore.QRect(20, 170, 261, 121))
         self.groupBox_init.setStyleSheet(_fromUtf8("QGroupBox{border: 1px solid rgb(192, 192, 192); border-radius:9px; font: bold}+ QGroupBox::title{subcontrol-origin: margin; left: 10px; padding: 0px 3px 0 3px}"))
@@ -98,6 +133,8 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.lineEdit_w2 = QtGui.QLineEdit(self.groupBox_init)
         self.lineEdit_w2.setGeometry(QtCore.QRect(170, 80, 71, 27))
         self.lineEdit_w2.setObjectName(_fromUtf8("lineEdit_w2"))
+
+        # time setting group
         self.groupBox_time = QtGui.QGroupBox(self.settingFrame)
         self.groupBox_time.setGeometry(QtCore.QRect(20, 310, 261, 131))
         self.groupBox_time.setStyleSheet(_fromUtf8("QGroupBox{border: 1px solid rgb(192, 192, 192); border-radius:9px; font: bold}+ QGroupBox::title{subcontrol-origin: margin; left: 10px; padding: 0px 3px 0 3px}"))
@@ -120,54 +157,149 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.lineEdit_dt = QtGui.QLineEdit(self.groupBox_time)
         self.lineEdit_dt.setGeometry(QtCore.QRect(70, 90, 71, 27))
         self.lineEdit_dt.setObjectName(_fromUtf8("lineEdit_dt"))
+
+        # pushButton
         self.pushButton_clear = QtGui.QPushButton(self.settingFrame)
         self.pushButton_clear.setGeometry(QtCore.QRect(60, 480, 99, 27))
         self.pushButton_clear.setObjectName(_fromUtf8("pushButton_clear"))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(
+            _fromUtf8("Icon/clear.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_clear.setIcon(icon)
+        self.pushButton_clear.setIconSize(QtCore.QSize(20, 20))
+
+
         self.pushButton_start = QtGui.QPushButton(self.settingFrame)
         self.pushButton_start.setGeometry(QtCore.QRect(180, 480, 99, 27))
         self.pushButton_start.setObjectName(_fromUtf8("pushButton_start"))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(
+            _fromUtf8("Icon/tick.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_start.setIcon(icon)
+        self.pushButton_start.setIconSize(QtCore.QSize(20, 20))
+
+        # vtk widget
+        self.vtk_widget = QtGui.QWidget(self.centralWidget)
+        self.vtk_widget.setGeometry(QtCore.QRect(320, 10, 431, 581))
+        self.vtk_widget.setObjectName(_fromUtf8("vtk_widget"))
+
+
+        # plot_widget
         self.plot_widget = QtGui.QWidget(self.centralWidget)
         self.plot_widget.setGeometry(QtCore.QRect(760, 10, 421, 581))
-        self.plot_widget.setObjectName(_fromUtf8("plot_widget"))
-        MainWindow.setCentralWidget(self.centralWidget)
+        self.plot_widget.setObjectName(_fromUtf8("plot_widget"))   
+
+        # menuBar
         self.menuBar = QtGui.QMenuBar(MainWindow)
         self.menuBar.setGeometry(QtCore.QRect(0, 0, 1203, 25))
         self.menuBar.setObjectName(_fromUtf8("menuBar"))
         MainWindow.setMenuBar(self.menuBar)
+
+        # toolbar
         self.mainToolBar = QtGui.QToolBar(MainWindow)
         self.mainToolBar.setIconSize(QtCore.QSize(50, 50))
         self.mainToolBar.setObjectName(_fromUtf8("mainToolBar"))
+        self.mainToolBar.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
+
         self.actionStart = QtGui.QAction(MainWindow)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/Icon/start.png")), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("Icon/start.png")), QtGui.QIcon.Normal, QtGui.QIcon.On)
         self.actionStart.setIcon(icon)
         self.actionStart.setObjectName(_fromUtf8("actionStart"))
         self.mainToolBar.addAction(self.actionStart)
 
+        self.actionExit = QtGui.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(
+            QtGui.QPixmap(_fromUtf8("Icon/exit.png")), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.actionExit.setIcon(icon)
+        self.actionExit.setObjectName(_fromUtf8("actionExit"))
+        self.mainToolBar.addAction(self.actionExit)
+
+        #
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
+        # mainwindow
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+
+        # system parameter setting group
         self.groupBox_system.setTitle(_translate("MainWindow", "System parameters", None))
         self.label_M1.setText(_translate("MainWindow", "M1", None))
         self.label_M2.setText(_translate("MainWindow", "M2", None))
         self.label_L1.setText(_translate("MainWindow", "L1", None))
         self.label_L2.setText(_translate("MainWindow", "L2", None))
+
+        # initial condition group
         self.groupBox_init.setTitle(_translate("MainWindow", "Initial conditions", None))
         self.label_th1.setText(_translate("MainWindow", "th1", None))
         self.label_th2.setText(_translate("MainWindow", "th2", None))
         self.label_w1.setText(_translate("MainWindow", "w1", None))
         self.label_w2.setText(_translate("MainWindow", "w2", None))
+
+        # time advancement group
         self.groupBox_time.setTitle(_translate("MainWindow", "Time advancement", None))
         self.label_ts.setText(_translate("MainWindow", "tstart", None))
         self.label_te.setText(_translate("MainWindow", "tend", None))
         self.label_dt.setText(_translate("MainWindow", "tstep", None))
+
+        # pushBotton
         self.pushButton_clear.setText(_translate("MainWindow", "Clear", None))
         self.pushButton_start.setText(_translate("MainWindow", "Start", None))
+
+        # toolbar
         self.actionStart.setText(_translate("MainWindow", "Start", None))
         self.actionStart.setToolTip(_translate("MainWindow", "Start", None))
+
+        self.actionExit.setText(_translate("MainWindow", "Exit", None))
+        self.actionExit.setToolTip(_translate("MainWindow", "Exit", None))
+
+
+    # other class member function
+    def get_input(self):
+        """ get input data from the GUI """
+        self.M1 = self.lineEdit_M1.text().toDouble()[0]
+        self.M2 = self.lineEdit_M2.text().toDouble()[0]
+        self.L1 = self.lineEdit_L1.text().toDouble()[0]
+        self.L2 = self.lineEdit_L2.text().toDouble()[0]
+
+        self.th1 = self.lineEdit_th1.text().toDouble()[0]
+        self.th2 = self.lineEdit_th2.text().toDouble()[0]
+        self.w1 = self.lineEdit_w1.text().toDouble()[0]
+        self.w2 = self.lineEdit_w2.text().toDouble()[0]
+
+        self.ts = self.lineEdit_ts.text().toDouble()[0]
+        self.te = self.lineEdit_te.text().toDouble()[0]
+        self.dt = self.lineEdit_dt.text().toDouble()[0]
+
+    def timerCallback(self):
+        """ timer callback function """
+        # update the current time
+        self.timer_count += 1
+
+
+
+
+
+    # toolbar slot function
+    @QtCore.pyqtSlot() # signal with no arguments
+    def on_actionStart_triggered(self):
+        """Toolbar start button slot"""
+        # get input data from the GUI
+        self.get_input()
+
+        # create pendulum object
+        self.pendulum = DoublePendulum(self.M1, self.M2, self.L1, self.L2)
+
+        # start time advancement process
+        self.timer.start(self.tstep*1000.0)     # convert time unit from s to ms
+
+    @QtCore.pyqtSlot() # signal with no arguments
+    def on_actionExit_triggered(self):
+        """Toolbar exit button slot"""
+        self.close()
 
 
 if __name__ == '__main__':
